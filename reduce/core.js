47c9b4341e562b848155f881ad5c4a1886530cde
agConvoy <alex.convoy@talktalk.net>
2022-03-24 11:43:15 +0000

    main

--- reduce/core.js
+++ reduce/core.js
@@ -6,9 +6,8 @@ const numArr = [1, 2, 3, 4, 5];
 const sum = numArr.reduce((prev, curr) => prev + curr);
 console.log(sum);
 
-function productAll(array) {
-  // your code here
-}
+const prod = numArr.reduce((prev, curr) => prev * curr);
+console.log(prod);
 
 function objectify(array) {
   // your code here
