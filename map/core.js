156edba0999ec97d63ed67de49f9c9e66e88a00f
agConvoy <alex.convoy@talktalk.net>
2022-03-24 11:13:50 +0000

    main

--- map/core.js
+++ map/core.js
@@ -1,22 +1,26 @@
-function multiplyBy10 (array) {
- // your code here
-};
+const numArr = [1, 2, 3, 4, 5];
 
-function shiftRight (array) {
- // your code here
-};
+let multipliedByTen = numArr.map(function (num) {
+  const multiplied = num * 10;
+  return multiplied;
+});
 
-function onlyVowels (array) {
- // your code here
-};
+console.log(multipliedByTen);
+function shiftRight(array) {
+  // your code here
+}
 
-function doubleMatrix (array) {
- // your code here
-};
+function onlyVowels(array) {
+  // your code here
+}
+
+function doubleMatrix(array) {
+  // your code here
+}
 
 module.exports = {
   multiplyBy10: multiplyBy10,
   shiftRight: shiftRight,
   onlyVowels: onlyVowels,
-  doubleMatrix: doubleMatrix
+  doubleMatrix: doubleMatrix,
 };
--- reduce/core.js
+++ reduce/core.js
@@ -1,24 +1,26 @@
-function sum (array) {
+// function sum (array) {
+//   // your code here
+// }
+
+const numArr = [1, 2, 3, 4, 5];
+const sum = numArr.reduce((prev, curr) => prev + curr);
+console.log(sum);
+
+function productAll(array) {
   // your code here
 }
 
-function productAll (array) {
+function objectify(array) {
   // your code here
 }
 
-function objectify (array) {
-  // your code here
-
-}
-
-function luckyNumbers (array) {
+function luckyNumbers(array) {
   // your code here
 }
 
-
-module.exports = {
-  sum: sum,
-  productAll: productAll,
-  objectify: objectify,
-  luckyNumbers: luckyNumbers
-};
+// module.exports = {
+//   sum: sum,
+//   productAll: productAll,
+//   objectify: objectify,
+//   luckyNumbers: luckyNumbers
+// };
